cb9d1d00bb9bca086e40ffe79746d4ff0e5fdf04
Dale Nguyen <dnguyen@ci.com>
2019-08-13 16:07:12 -0400

    Added speakers logic

--- speakers-service/config/index.js
+++ speakers-service/config/index.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const bunyan = require('bunyan');
+
 // Load package.json
 const pjs = require('../package.json');
 
@@ -14,18 +16,27 @@ module.exports = {
     name,
     version,
     serviceTimeout: 30,
+    data: {
+      speakers: path.join(__dirname, '../data/speakers.json'),
+    },
     log: () => getLogger(name, version, 'debug'),
   },
   production: {
     name,
     version,
     serviceTimeout: 30,
+    data: {
+      speakers: path.join(__dirname, '../data/speakers.json'),
+    },
     log: () => getLogger(name, version, 'info'),
   },
   test: {
     name,
     version,
     serviceTimeout: 30,
+    data: {
+      speakers: path.join(__dirname, '../data/speakers.json'),
+    },
     log: () => getLogger(name, version, 'fatal'),
   },
 };
