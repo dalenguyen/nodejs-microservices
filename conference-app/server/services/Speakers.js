faea5d4e22ad607b2b98664751b8f5dffb3b2460
Dale Nguyen <dungnq@itbox4vn.com>
2019-08-13 23:27:30 -0400

    Used cache to save data in case of service interuption

--- conference-app/server/services/Speakers.js
+++ conference-app/server/services/Speakers.js
@@ -1,5 +1,7 @@
 /* eslint-disable class-methods-use-this */
+const url = require('url');
 const axios = require('axios');
+const crypto = require('crypto');
 
 const CircuitBreaker = require('../lib/CircuitBreaker');
 const circuitBreaker = new CircuitBreaker();
@@ -8,6 +10,7 @@ class SpeakersService {
   constructor({ serviceRegistryUrl, serviceVersionIdentifier }) {
     this.serviceRegistryUrl = serviceRegistryUrl;
     this.serviceVersionIdentifier = serviceVersionIdentifier;
+    this.cache = {};
   }
 
   async getImage(path) {
@@ -68,7 +71,18 @@ class SpeakersService {
   }
 
   async callService(requestOptions) {
-   return circuitBreaker.callService(requestOptions);
+    const servicePath = url.parse(requestOptions.url).path;
+    const cacheKey = crypto.createHash('md5').update(requestOptions.method + servicePath).digest('hex');
+
+    const result = await circuitBreaker.callService(requestOptions);    
+
+    if (!result) {
+      if (this.cache[cacheKey]) return this.cache[cacheKey];
+      return false;
+    }
+
+    this.cache[cacheKey] = result;
+    return result;
   }
 
   async getService(servicename) {
